0a8a0b48fe966c9e82c677ec7d603aadf7e3a70e
Behzad Nouri <61915687+behnouri@users.noreply.github.com>
2021-01-30 00:39:02 +0100

    Create analysis.R

---
+++ analysis.R
@@ -0,0 +1,89 @@
+#---------Load Libraries ------------------------------------
+library(plyr)
+library(tidyverse)
+library(naniar)
+library(VIM)
+library(DMwR)
+library(caret)
+library(PerformanceAnalytics)
+library(visdat)
+library(stringr)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+
+
+#--------Load Data-------------------------------------------
+main_data <- read.csv("c:/excercise/excercise.csv", 
+                    quote = "",na.strings = c("", "NA"))
+colnames(main_data)[2] <- "freight"
+
+
+df <- main_data %>%
+  separate(Year.week, c("year", "week"), "/") %>%
+  mutate(week = as.numeric(week)) %>%
+  mutate(year = as.numeric(year)) %>%
+  filter(week %in% c(1:52)) %>%
+  mutate(freight = replace_na(freight, median(freight,na.rm = TRUE)))
+
+#check and impute NAs after done
+
+for(i in df$year){
+  for(j in 1:52){
+    if(!(j %in% df[which(df$year==i),c("week")])){
+      print(paste("year" , i , "week" , j))
+      break
+    }
+  }
+}
+
+miss<-data.frame(c(2017,2018),c(13,21),c(0,0))
+names(miss)<-names(df)
+newdf <- rbind(df, miss)
+
+
+newdf <- newdf %>%
+  arrange(year,week) %>%
+  mutate(year_week = paste(week,"-",year)) %>%
+  mutate(year_week = gsub(" ", "", year_week)) %>%
+  mutate(date= as.Date(paste(newdf$year, newdf$week, 1, sep="-"), "%Y-%U-%u")) %>%
+  mutate(freight = ifelse(freight==0, median(freight,na.rm = TRUE),freight))
+
+#check for NA impute
+
+sum(is.na(df))
+
+
+
+
+# Make the plot
+p <- ggplot(newdf[313:468,], aes(x=date, y=freight)) +
+  geom_line(color="red", size=1.1) +
+  theme_minimal() +
+  xlab("") +
+  ylab("Consumer Price Index") +
+  ggtitle("Indonesia's Consumer Price Index", subtitle = "From December 2002 Until April 2020")
+p
+
+
+# Get the statistical summary
+# Returns data frame and sort based on the CPI
+newdf %>%
+  arrange(desc(freight))
+
+
+ts1 <- ts(newdf$freight, frequency = 52, start = c(2012, 1))
+ts1
+
+
+#plot.ts(ts1)
+
+#as.Date(paste(df$year, df$week, 1, sep="-"), "%Y-%U-%u")
+
+
+acf(ts1, plot = T, main = "ACF Plot of CPI", xaxt="n")
+pacf(ts1, plot = T, main = "ACF Plot of CPI", xaxt="n")
+
+
+
