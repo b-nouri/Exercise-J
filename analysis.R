34d93e333dd4b6d0efa9e8a9dab13de4f153778e
Behzad Nouri <61915687+behnouri@users.noreply.github.com>
2021-01-30 01:20:13 +0100

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -47,7 +47,7 @@ newdf <- newdf %>%
   arrange(year,week) %>%
   mutate(year_week = paste(week,"-",year)) %>%
   mutate(year_week = gsub(" ", "", year_week)) %>%
-  mutate(date= as.Date(paste(newdf$year, newdf$week, 1, sep="-"), "%Y-%U-%u")) %>%
+  mutate(date= as.Date(paste(newdf$year, newdf$week, 1, sep="-"), "%Y-%W-%u")) %>%
   mutate(freight = ifelse(freight==0, median(freight,na.rm = TRUE),freight))
 
 #check for NA impute
@@ -58,7 +58,17 @@ sum(is.na(df))
 
 
 # Make the plot
-p <- ggplot(newdf[313:468,], aes(x=date, y=freight)) +
+for(i in 1:9){
+  p <- ggplot(newdf[((i-1)*52+ 1):((i-1)*52+ 51),], aes(x=date, y=freight)) +
+    geom_line(color="red", size=1.1) +
+    theme_minimal() +
+    xlab("") +
+    ylab("Fright size") +
+    ggtitle("freight units being shipped Westbound (RTM to KGH)", 
+            subtitle = paste("From January", 2011+i, "Until December", 2011+i))
+  print(p)
+}
+p <- ggplot(newdf[,], aes(x=date, y=freight)) +
   geom_line(color="red", size=1.1) +
   theme_minimal() +
   xlab("") +
@@ -76,7 +86,9 @@ newdf %>%
 ts1 <- ts(newdf$freight, frequency = 52, start = c(2012, 1))
 ts1
 
-
+library(tseries)
+adf.test(ts1) # p-value < 0.05 indicates the TS is stationary
+kpss.test(ts1)
 #plot.ts(ts1)
 
 #as.Date(paste(df$year, df$week, 1, sep="-"), "%Y-%U-%u")
